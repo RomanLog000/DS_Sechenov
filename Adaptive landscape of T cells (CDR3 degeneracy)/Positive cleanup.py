e7cac738a375f6f5424b688bfad7888a3c93b9d2
RomanLog000 <logosmr000@gmail.com>
2025-01-10 10:29:02 +0800

    upd

--- Adaptive landscape of T cells (CDR3 degeneracy)/Positive cleanup.py
+++ Adaptive landscape of T cells (CDR3 degeneracy)/Positive cleanup.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Загрузка данных
-pos_file = r"C:\Users\Роман\Documents\Science\Moscow\Ph.D\Olya\YFV_cdr3_sequences_pos_B_filtered.csv"
-neg_file = r"C:\Users\Роман\Documents\Science\Moscow\Ph.D\Olya\YFV_cdr3_sequences_neg_B_filtered.csv"
+pos_file = r"C:\Users\Роман\Documents\Science\Moscow\Ph.D\Olya\YFV_cdr3_sequences_pos_A_filtered.csv"
+neg_file = r"C:\Users\Роман\Documents\Science\Moscow\Ph.D\Olya\YFV_cdr3_sequences_neg_A_filtered.csv"
 
 # Чтение файлов
 pos_data = pd.read_csv(pos_file, header=None, names=["Sequence"])
@@ -12,4 +12,4 @@ neg_data = pd.read_csv(neg_file, header=None, names=["Sequence"])
 unique_pos_data = pos_data[~pos_data["Sequence"].isin(neg_data["Sequence"])]
 
 # Сохранение результата
-unique_pos_data.to_csv("YFV_cdr3_sequences_pos_B_filtered_unique.csv", index=False, header=False)
+unique_pos_data.to_csv("YFV_cdr3_sequences_pos_A_filtered_unique.csv", index=False, header=False)
